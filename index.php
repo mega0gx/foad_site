3f621116acb1f27d11c9254a7d50ba283ec360b7
mega0gx <mega0gx@gmail.com>
2025-01-12 08:35:35 +0200

    fix issues

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/about.css">
+    <link rel="stylesheet" href="./assets/css/services.css">
 </head>
 <body>
     <?php include './sections/header.html'; ?>
